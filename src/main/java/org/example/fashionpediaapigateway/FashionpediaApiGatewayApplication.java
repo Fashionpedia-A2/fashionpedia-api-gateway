92ad3ceb28d07d587a4e050b9de3d7d035f489d8
Hamdi <ilhamabdillah123@gmail.com>
2024-04-16 17:46:41 +0700

    Init

---
+++ src/main/java/org/example/fashionpediaapigateway/FashionpediaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.fashionpediaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionpediaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionpediaApiGatewayApplication.class, args);
+	}
+
+}
